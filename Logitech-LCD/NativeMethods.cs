afc1aac4c23d511db665d0e28c46915e36098f0d
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2014-03-25 19:43:12 +0100

    Renamed Class1 -> NativeMethods Created necessary enums to use SDK

--- Logitech-LCD/NativeMethods.cs
+++ Logitech-LCD/NativeMethods.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Logitech_LCD
 {
+    /// <summary>
+    /// Class containing necessary informations and calls to the Logitech SDK
+    /// </summary>
     public class NativeMethods
     {
+        #region Enumerations
+        /// <summary>
+        /// LCD Types
+        /// </summary>
+        [Flags]
+        public enum LcdType
+        {
+            Mono = 0x1,
+            Color = 0x2,
+        }
+
+        /// <summary>
+        /// Screen buttons
+        /// </summary>
+        [Flags]
+        public enum Buttons
+        {
+            MonoButton0 = 0x1,
+            ManoButton1 = 0x2,
+            MonoButton2 = 0x4,
+            MonoButton3 = 0x8,
+            ColorLeft = 0x100,
+            ColorRight = 0x200,
+            ColorOK = 0x400,
+            ColorCancel = 0x800,
+            ColorUp = 0x1000,
+            ColorDown = 0x2000,
+            ColorMenu = 0x4000,
+        }
+        #endregion
     }
 }

db3ae4e58d4492e0fa45b0c2527fa7a544f912b4
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2014-03-26 20:36:47 +0100

    Added Enums

--- Logitech-LCD/NativeMethods.cs
+++ Logitech-LCD/NativeMethods.cs
@@ -1,11 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Logitech_LCD
 {
+
+    public enum MonoBitmap
+    {
+        Width = 160,
+        Height = 43,
+        Bpp = 1,
+    }
+
+    public enum ColorBitmap
+    {
+        Width = 320,
+        Height = 240,
+        Bpp = 4,
+    }
+    public enum ReturnValue
+    {
+        ErrorSuccess = 0,
+        ErrorFileNotFound = 2,
+        ErrorAccessDenied = 5,
+        ErrorInvalidParameter = 87,
+        ErrorLockFailed = 167,
+        ErrorAlreadyExists = 183,
+        ErrorNoMoreItems = 259,
+        ErrorOldWinVersion = 1150,
+        ErrorServiceNotActive = 1062,
+        ErrorDeviceNotConnected = 1167,
+        ErrorAlreadyInitialized = 1247,
+        ErrorNoSystemResources = 1450,
+        RcpSServerUnavailable = 1722,
+        RcpXWrongPipeVersion = 1832,
+    }
+
     /// <summary>
     /// Class containing necessary informations and calls to the Logitech SDK
     /// </summary>

2784ba201814fea060b6af2af0ca10c2f379be53
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2014-03-27 23:21:32 +0100

    Added a default constructor which will try to guess wich type of keyboard is connected. If none, the component is deactivated

--- Logitech-LCD/Applets/BaseApplet.cs
+++ Logitech-LCD/Applets/BaseApplet.cs
@@ -7,48 +7,91 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
+using System.ComponentModel;
+using Logitech_LCD.Exceptions;
 
 namespace Logitech_LCD.Applets
 {
-    public abstract class BaseApplet : UserControl
+    public class BaseApplet : UserControl
     {
-        private int _height;
-        private int _width;
-        private int _bpp;
-        private LcdType _lcdType;
+        protected int _height;
+        protected int _width;
+        protected int _bpp;
+        protected LcdType? _lcdType;
+
+        public event EventHandler dataUpdate;
 
         private Timer _updatetTimer;
-        public BaseApplet(LcdType lcdType)
+
+        private static LcdType? detectLcdType()
         {
-            _lcdType = lcdType;
-
-            _updatetTimer = new Timer();
-            _updatetTimer.Interval = 100 / 6;
-            _updatetTimer.Tick += updateGraphics;
-
-            if (lcdType == LcdType.Color)
+            try
             {
-                _height = (int)ColorBitmap.Height;
-                _width = (int)ColorBitmap.Width;
-                _bpp = (int)ColorBitmap.Bpp;
+                if (LogitechLcd.Instance.isConnected(LcdType.Color))
+                {
+                    return LcdType.Color;
+                }
+                else if (LogitechLcd.Instance.isConnected(LcdType.Mono))
+                {
+                    return LcdType.Mono;
+                }
+                else
+                {
+                    return null;
+                }
             }
-            else if (lcdType == LcdType.Mono)
+            catch (LcdNotInitializedException)
             {
-                _height = (int)MonoBitmap.Height;
-                _width = (int)MonoBitmap.Width;
-                _bpp = (int)MonoBitmap.Bpp;
+#if !DEBUG
+                //Emergency initialization
+                LogitechLcd.Instance.init("", LcdType.Color | LcdType.Mono);
+                return detectLcdType();
+#else
+                return null;
+#endif
             }
-            else
+        }
+
+        public BaseApplet()
+            : this(detectLcdType())
+        {
+
+        }
+
+        public BaseApplet(LcdType? lcdType)
+        {
+            if (lcdType != null)
             {
-                throw new ArgumentException("Bad LCD Type", "lcdType");
+                _lcdType = lcdType;
+
+                _updatetTimer = new Timer();
+                _updatetTimer.Interval = 100 / 6;
+                _updatetTimer.Tick += updateGraphics;
+
+                if (lcdType == LcdType.Color)
+                {
+                    _height = (int)ColorBitmap.Height;
+                    _width = (int)ColorBitmap.Width;
+                    _bpp = (int)ColorBitmap.Bpp;
+                }
+                else if (lcdType == LcdType.Mono)
+                {
+                    _height = (int)MonoBitmap.Height;
+                    _width = (int)MonoBitmap.Width;
+                    _bpp = (int)MonoBitmap.Bpp;
+                }
+                else
+                {
+                    throw new ArgumentException("Bad LCD Type", "lcdType");
+                }
+                dataUpdate += OnDataUpdate;
+                _updatetTimer.Start();
             }
-
-            _updatetTimer.Start();
-
         }
 
         private void updateGraphics(object sender, EventArgs e)
         {
+            this.dataUpdate(this, EventArgs.Empty);
             PixelFormat format;
             if (_bpp == ((int)MonoBitmap.Bpp))
             {
@@ -80,6 +123,12 @@ namespace Logitech_LCD.Applets
             {
                 LogitechLcd.Instance.monoSetBackground(pixels);
             }
+            LogitechLcd.Instance.update();
+        }
+
+        protected virtual void OnDataUpdate(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }

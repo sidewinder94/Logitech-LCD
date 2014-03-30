b4e1043f5b05bfb7f3a9f4f5becfce2c741ad37f
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2014-03-30 03:42:29 +0200

    Cleaned & Sorted Usings

--- Logitech-LCD/LogitechLcd.cs
+++ Logitech-LCD/LogitechLcd.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Logitech_LCD.Exceptions;
+﻿using Logitech_LCD.Exceptions;
+using System;
 
 namespace Logitech_LCD
 {
